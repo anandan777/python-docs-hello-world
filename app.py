0c8662a41dc35f11cf80566082c5a7f61e4444a6
anandan777 <146792635+anandan777@users.noreply.github.com>
2025-01-30 13:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, Response, redirect
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route('/cdn-images/stock/y/yf17vwj-7-l.jpg')
 def fake_image():
-    js_code = "alert('Image is not available! Redirecting...'); window.location.href='/cdn-images/stock/y/yf17vwj-7-l-real.jpg';"
-    return Response(js_code, mimetype='application/javascript')
+    html_content = """
+    <html>
+        <body>
+            <script>
+                alert('Image is not available! Redirecting...');
+                window.location.href = '/cdn-images/stock/y/yf17vwj-7-l-real.jpg';
+            </script>
+            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==" style="display:none;">
+        </body>
+    </html>
+    """
+    return Response(html_content, mimetype='text/html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
